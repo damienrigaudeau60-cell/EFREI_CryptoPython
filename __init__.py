52e20d2ff1e9a0e38ea67f3c3df2bffac4768923
damienrigaudeau60-cell <damien.rigaudeau60@gmail.com>
2026-03-15 18:59:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,18 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Génération de la clé de chiffrement et création de l'objet Fernet0
 key = Fernet.generate_key()
 f = Fernet(key)
 
-@app.route('/')
-def hello_world():
-    return index('index.html')
-
-@app.route('templates/site/index.html')
+@app.route("/")
 def index():
-     return render_template('index.html')
+    return render_template("templates/site/index.html")
 
-                                                                                                                                                     
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run()
